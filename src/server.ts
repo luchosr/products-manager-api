32c6a91017680ea8f978906203256c17bb512d07
Luciano Ramello <luchosr@gmail.com>
2024-11-08 20:18:39 +0100

    feat: improves server

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import express from 'express';
+import router from './router';
 
 const server = express();
 
-//routing
+server.use('/products', router);
 
-server.get('/', (req, res) => {
-  const datos = [
-    {
-      id: 1,
-      name: 'Fernando',
-    },
-    {
-      id: 2,
-      name: 'Luciano',
-    },
-  ];
-  res.send(datos);
-});
+//routing
 
 export default server;
