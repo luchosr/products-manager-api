823380ea23a9ebea9a5796a4f7d477169e2cef29
Luciano Ramello <luchosr@gmail.com>
2024-11-09 19:04:36 +0100

    feat: adds datrabase connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from 'express';
 import router from './router';
+import db from './config/db';
 
+// Connect to DB
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    console.log('Database connected');
+  } catch (error) {
+    console.log(error);
+    console.log('Error connecting to DB');
+  }
+}
+
+connectDB();
 const server = express();
 
 server.use('/products', router);
