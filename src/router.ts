a0beaeeb779e27814b3f88857d25cc5d65ddc534
Luciano Ramello <luchosr@gmail.com>
2024-11-11 18:17:37 +0100

    feat: implements post handler

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { createProduct } from './handlers/products';
 
 const router = Router();
 
@@ -6,9 +7,7 @@ router.get('/', (req, res) => {
   res.json('Desde GET');
 });
 
-router.post('/', (req, res) => {
-  res.json('Desde POST');
-});
+router.post('/', createProduct);
 
 router.put('/', (req, res) => {
   res.json('Desde PUT');
