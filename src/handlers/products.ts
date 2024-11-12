15f19dae47b580131f43849275e3a647865fc258
Luciano Ramello <luchosr@gmail.com>
2024-11-12 18:34:14 +0100

    feat: adds products handlers

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,8 +1,61 @@
 import { Request, Response } from 'express';
 import Product from '../models/Product.model';
 
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({
+      order: [['price', 'DESC']],
+      attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] },
+    });
+    res.json({ data: products });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+export const getProductsById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 export const createProduct = async (req: Request, res: Response) => {
-  const product = await Product.create(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  await product.update(req.body);
+  await product.save();
+  res.json({ data: product });
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  await product.update(req.body);
+  await product.save();
   res.json({ data: product });
 };
