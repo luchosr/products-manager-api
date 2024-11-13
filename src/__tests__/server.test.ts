ae7921d671651f6f1883d99253e0531ed40c56a3
Luciano Ramello <luchosr@gmail.com>
2024-11-13 10:26:35 +0100

    test: adds server test

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,9 @@
+describe('nuestro primer teto', () => {
+  it('uno mas uno = 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it('uno mas uno = 2', () => {
+    expect(1 + 1).not.toBe(3);
+  });
+});
