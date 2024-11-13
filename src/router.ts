f8b1c93ce5716eb092745074e3142491407f84d0
Luciano Ramello <luchosr@gmail.com>
2024-11-13 11:20:18 +0100

    feat: updates router

--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,8 @@ import { body, param } from 'express-validator';
 import {
   createProduct,
   deleteProduct,
+  getProductById,
   getProducts,
-  getProductsById,
   updateAvailability,
   updateProduct,
 } from './handlers/products';
@@ -12,57 +12,67 @@ import { handleInputErrors } from './middleware';
 
 const router = Router();
 
+// Routing
 router.get('/', getProducts);
 router.get(
   '/:id',
-  param('id').isNumeric().withMessage('Id must be numeric'),
+  param('id').isInt().withMessage('ID no válido'),
+  //@ts-ignore
   handleInputErrors,
-  getProductsById
+  getProductById
 );
 
 router.post(
   '/',
-
-  body('name').notEmpty().withMessage('Name is required'),
+  // Validación
+  body('name')
+    .notEmpty()
+    .withMessage('El nombre de Producto no puede ir vacio'),
   body('price')
     .isNumeric()
-    .withMessage('Value must be numeric')
+    .withMessage('Valor no válido')
     .notEmpty()
-    .withMessage('Value cannot be empty')
+    .withMessage('El precio de Producto no puede ir vacio')
     .custom((value) => value > 0)
-    .withMessage('Price is not valid'),
+    .withMessage('Precio no válido'),
+  //@ts-ignore
   handleInputErrors,
   createProduct
 );
 
 router.put(
   '/:id',
-  param('id').isNumeric().withMessage('Id must be numeric'),
-  body('name').notEmpty().withMessage('Name is required'),
+  param('id').isInt().withMessage('ID no válido'),
+  body('name')
+    .notEmpty()
+    .withMessage('El nombre de Producto no puede ir vacio'),
   body('price')
     .isNumeric()
-    .withMessage('Value must be numeric')
+    .withMessage('Valor no válido')
     .notEmpty()
-    .withMessage('Value cannot be empty')
+    .withMessage('El precio de Producto no puede ir vacio')
     .custom((value) => value > 0)
-    .withMessage('Price is not valid'),
+    .withMessage('Precio no válido'),
   body('availability')
     .isBoolean()
-    .withMessage('Availability value is not valid'),
+    .withMessage('Valor para disponibilidad no válido'),
+  //@ts-ignore
   handleInputErrors,
   updateProduct
 );
 
 router.patch(
   '/:id',
-  param('id').isNumeric().withMessage('Id must be numeric'),
+  param('id').isInt().withMessage('ID no válido'),
+  //@ts-ignore
   handleInputErrors,
   updateAvailability
 );
 
 router.delete(
   '/:id',
-  param('id').isNumeric().withMessage('Id must be numeric'),
+  param('id').isInt().withMessage('ID no válido'),
+  //@ts-ignore
   handleInputErrors,
   deleteProduct
 );
